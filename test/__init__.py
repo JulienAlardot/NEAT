f2252d08efcbc630b4573c0be27a2691accfc6e9
Julien Alardot (jual) <jual@odoo.com>
2022-09-21 13:33:10 +0200

    [IMP] NEATBaseTestCase: increasing maxDiff to 50k
    Signed-off-by: Julien Alardot (jual) <jual@odoo.com>

--- test/__init__.py
+++ test/__init__.py
@@ -5,6 +5,6 @@ from core.database import Database
 
 class NEATBaseTestCase(TestCase):
     def setUp(self):
-        self.maxDiff = 5000
+        self.maxDiff = 50000
         self._db = Database('test/test', override=True)
         self._db.execute("""INSERT INTO model_metadata DEFAULT VALUES""")
