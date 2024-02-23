46a6066a86a187cdfbdb8cc8d4591e8c7e0fc4c1
Julien Alardot (jual) <alardotj.pro@gmail.com>
2024-02-23 19:45:08 +0100

    [IMP] activation: Add a test for the leaky relu
    Signed-off-by: Julien Alardot (jual) <alardotj.pro@gmail.com>

--- test/test_activation.py
+++ test/test_activation.py
@@ -1,15 +1,24 @@
 from unittest import TestCase
 
-from core.activation import sigmoid, relu
+from core.activation import sigmoid, relu, EPSILON, leaky_relu
 
 
 class TestActivation(TestCase):
     def test_sigmoid(self):
+        """ Tests sigmoid activation function """
+
         self.assertAlmostEqual(0, sigmoid(-10), 6)
         self.assertAlmostEqual(0.5, sigmoid(0), 6)
         self.assertAlmostEqual(1, sigmoid(10), 6)
     
     def test_relu(self):
+        """ Tests ReLu activation function """
         self.assertEqual(0, relu(0))
         self.assertEqual(1, relu(1))
         self.assertEqual(0, relu(-1))
+    
+    def test_lrelu(self):
+        """ Tests Leaky ReLu activation function """
+        self.assertEqual(0, leaky_relu(0))
+        self.assertEqual(1, leaky_relu(1))
+        self.assertEqual(-EPSILON, leaky_relu(-1))
